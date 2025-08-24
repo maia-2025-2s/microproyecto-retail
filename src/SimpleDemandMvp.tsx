67c6674b3e83cde072982d1055648a3428afcaf3
Dammonx66 <pf.rengifo10@uniandes.edu.co>
2025-08-24 01:14:46 -0500

    Limpieza: elimino scaffolding de Vite/React, dejo solo DVC

--- src/SimpleDemandMvp.tsx
+++
@@ -1,1 +0,0 @@
-import React, { useMemo, useState, useEffect } from "react";
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SimpleDemandMvp from './SimpleDemandMvp'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SimpleDemandMvp />
-  </React.StrictMode>
-)
