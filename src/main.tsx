a02d7714aa8110d88472b7134835c343af7ef20f
vorellana-uniandes <v.orellana@uniandes.edu.co>
2025-09-07 03:15:26 -0300

    [vorellan] - [feat]: [] - [add main]
    -
    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SimpleDemandMvp from './SimpleDemandMvp'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SimpleDemandMvp />
+    <App />
   </React.StrictMode>
 )
